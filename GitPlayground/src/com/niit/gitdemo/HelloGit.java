89017670551c5d7b89fddf3293812bb5989b7a97
Training <Training@192.168.253.9>
2020-03-07 14:50:11 +0530

    Version 1.2

--- GitPlayground/src/com/niit/gitdemo/HelloGit.java
+++ GitPlayground/src/com/niit/gitdemo/HelloGit.java
@@ -1,6 +1,6 @@
 package com.niit.gitdemo;
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("hello this is next version.");
+		System.out.println("hello this is new version.");
 	}
 }
