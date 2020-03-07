205ec47e53a95ea401bdddf5ea35b6fd704351b7
Training <Training@192.168.253.9>
2020-03-07 15:47:44 +0530

    new demo

---
+++ GitPlayground/src/com/niit/gitdemo/Demo.java
@@ -0,0 +1,8 @@
+package com.niit.gitdemo;
+
+public class Demo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello I have made a change!");
+	}
+}
